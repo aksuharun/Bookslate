6c5807a250866ab32babdbbda2dad9de86b26487
Harun Aksu <harun16aksu@gmail.com>
2024-05-06 19:56:33 +0300

    Remove unnecessary 'try...catch' statement in base-service.js

--- services/base-service.js
+++ services/base-service.js
@@ -1,44 +1,24 @@
 class Service {
 	
 	async add(item){
-		try{
-			return await this.model.create(item)
-		}
-		catch(err){
-			throw err
-		}
+		return await this.model.create(item)
 	}
 	
 	async findAll(){
-			return await this.model.find()
+		return await this.model.find()
 	}
 
 	async find(id){
 		return await this.model.findById(id)
+
 	}
 	
 	async update(id, item){
-		if(await this.find(id)){
-			try{
-				return await this.model.findByIdAndUpdate(id, item, {new: true}) // {new: true} returns the updated document
-			}catch(err){
-				throw err
-			}
-		}
+		return await this.model.findByIdAndUpdate(id, item, {new: true}) // {new: true} returns the updated document
 	}
 	
 	async del(id){
-		if(await this.find(id)){
-			try{
-				return await this.model.findByIdAndDelete(mongoose)
-			}
-			catch(err){
-				throw err
-			}
-		}
-		else{
-			throw new Error('Item not found')
-		}
+		return await this.model.findByIdAndDelete(mongoose)
 	}
 
 }
