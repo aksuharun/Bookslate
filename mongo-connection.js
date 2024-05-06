846fb4e56c7618eae038e0d938ef6ba705dc47aa
Harun Aksu <harun16aksu@gmail.com>
2024-05-06 15:19:31 +0300

    Add error handling in mongo-connection.js

--- mongo-connection.js
+++ mongo-connection.js
@@ -1,8 +1,14 @@
 import mongoose from 'mongoose'
 
 async function main(){
-	await mongoose.connect('mongodb://localhost:27017/test')
-	console.log('Connected to MongoDB!')
+	try{
+		await mongoose.connect('mongodb://localhost:27017/test')
+		console.log('Connected to MongoDB!')
+	}
+	catch{
+		console.log('Cannot connect to MongoDB!')
+		setTimeout(main, 30000) // Try again in 30 seconds
+	}
 }
 
 main()
