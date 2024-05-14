313f5fd214ea60ea22b956a08a00c41c31acee3b
Harun Aksu <harun16aksu@gmail.com>
2024-05-14 12:49:01 +0300

    Fix Book Router and update related views and scripts

--- routes/book.js
+++ routes/book.js
@@ -2,6 +2,7 @@ import express from 'express'
 import LogService from '../services/log-service.js'
 import BookService from '../services/book-service.js'
 import CloudStorageService from '../services/cloud-storage-service.js'
+import LocalFileService from '../services/local-file-service.js'
 import { isAuthenticated, isAdmin, uploadHandler } from './middleware.js'
 
 const router = express.Router()
@@ -18,7 +19,6 @@ router.get('/all', async (req, res) => {
 			res.render('list', { items: books, itemType:'Book' })
 		})
 		.catch((err) => {
-			console.log(err)
 			res.status(500).json({ msg: 'Error getting books'})
 		
 		})
@@ -36,12 +36,28 @@ router.get('/all/json', async (req, res) => {
 		})
 })
 
-router.get('/update/:id', (req, res) => {
-	res.render('update-book', { id: req.params.id })
+router.get('/update/:id', async(req, res) => {
+	await BookService.find(req.params.id)
+		.then(book => {
+			console.log(book)
+			res.render('update-book', { book: book })
+		})
+		.catch((err) => {
+			console.log(err)
+			res.status(404).json({ msg: 'Book not found'})
+		
+		})
 })
 
-router.get('/delete', (req, res) => {
-	res.render('delete', { itemType: 'Book' })
+router.get('/delete/:id', async (req, res) => {
+	await BookService.find(req.params.id)
+		.then(book => {
+			res.render('delete', { id: book._id, itemType: 'Book' })			
+		})
+		.catch((err) => {
+			console.log(err)
+			res.status(404).json({ msg: 'Book not found'})
+		})
 })
 
 router.get('/:id', async (req, res) => {
@@ -92,7 +108,7 @@ router.post('/add', isAdmin, uploadHandler.fields(bookFields), async (req, res) 
 			[bookFile.filename, coverImageFile.filename],
 			'books/'
 		)
-
+		await LocalFileService.deleteFiles([bookFile.path, coverImageFile.path])
 		const book = await BookService.add({
 			title: req.body.title,
 			author: req.body.author,
@@ -101,7 +117,6 @@ router.post('/add', isAdmin, uploadHandler.fields(bookFields), async (req, res) 
 			fileUrl: 'books/' + bookFile.filename,
 			coverImageUrl: 'books/' + coverImageFile.filename,
 		})
-
 		await LogService.add({
 			userId: req.decoded._id,
 			action: 'Add',
@@ -122,16 +137,15 @@ router.put('/update/:id', isAdmin, uploadHandler.fields(bookFields) , async (req
 	try {
 
 		const book = await BookService.find(req.params.id)
-
-		await CloudStorageService.moveFiles(
+		await CloudStorageService.deleteFiles(
 			[book.fileUrl, book.coverImageUrl],
-			'old-books/'
 		)
 		await CloudStorageService.uploadFiles(
 			'uploads/',
 			[bookFile.filename, coverImageFile.filename],
 			'books/'
 		)
+		await LocalFileService.deleteFiles([bookFile.path, coverImageFile.path])
 		await BookService.update(req.params.id, {
 			title: req.body.title,
 			author: req.body.author,
@@ -155,15 +169,16 @@ router.put('/update/:id', isAdmin, uploadHandler.fields(bookFields) , async (req
 
 router.delete('/delete/:id', isAdmin, async (req, res) => {
 	try{
-		await CloudStorageService.deleteFile(book.fileUrl)
-		await CloudStorageService.deleteFile(book.coverImageUrl)
-		await BookService.delete(req.params.id)
+		const book = await BookService.find(req.params.id)
+		await CloudStorageService.deleteFiles([book.fileUrl, book.coverImageUrl])
+		await BookService.del(req.params.id)
 		await LogService.add({
 			userId: req.decoded._id,
 			action: 'Delete',
 			refType: 'Book',
 			refId: req.params.id,
 		})
+		res.json({ msg: 'Book deleted successfully'})
 	}catch(err){
 		console.log(err)
 		res.status(400).json({ msg: 'Error deleting book'})
--- views/script.js
+++ views/script.js
@@ -1,36 +1,34 @@
 const form = document.querySelector('form')
 
 form.addEventListener('submit', async (e) => {
-		e.preventDefault();
+    e.preventDefault();
 
-		const formData = new FormData(form);
-		let data = Object.fromEntries(formData.entries());
+    const formData = new FormData(form);
+    let data = Object.fromEntries(formData.entries());
 
-		if (data.name && data.surname) {
-				// Combine name and surname into a single name field
-				data.name = data.name + ' ' + data.surname;
-				delete data.surname;
-		}
+    if (data.name && data.surname) {
+        // Combine name and surname into a single name field
+        data.name = data.name + ' ' + data.surname;
+        delete data.surname;
+    }
 
-
-
-		if (form.getAttribute('method') === 'post') {
-				if (data.bookFile && data.coverImageFile) {
-						return await axios.post('', data, {
-								headers: {
-										'Content-Type': 'multipart/form-data'
-								}
-						}).then(console.log).catch(console.error)
-				}
-				return await axios.post('', data).then(console.log).catch(console.error)
-		}
-
-		if(data.bookFile && data.coverImageFile) {
-				return await axios.put('', data, {
-						headers: {
-								'Content-Type': 'multipart/form-data'
-						}
-				}).then(console.log).catch(console.error)
-		}
-		return await axios.put('', data).then(console.log).catch(console.error)
-	})
+    //check is there a input type = file
+    const headers = {}
+    if(form.querySelector('input[type="file"]')) {
+        headers['Content-Type'] = 'multipart/form-data'
+    }
+		
+    const method = form.getAttribute('method')
+    axios({
+        method: method,
+        url: '',
+        data: data,
+        headers: headers
+    })
+    .then(response => {
+        console.log(response)
+    })
+    .catch(error => {
+        console.error(error)
+    })
+})
