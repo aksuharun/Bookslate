9436b5a8249a6383580f23e9f9271859ea2dc6e8
Harun Aksu <harun16aksu@gmail.com>
2024-05-16 21:01:25 +0300

    Delete unnecessary tab

--- services/book-service.js
+++ services/book-service.js
@@ -2,7 +2,6 @@ import BaseService from './base-service.js';
 import BookModel from '../models/book.js';
 
 class BookService extends BaseService {
-		model = BookModel
+	model = BookModel
 }
-
 export default new BookService();
