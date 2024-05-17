413468cd32a230af21505766218e57bea3ca8bc3
Harun Aksu <harun16aksu@gmail.com>
2024-05-17 21:14:04 +0300

    Update book store

--- frontend/src/stores/book.js
+++ frontend/src/stores/book.js
@@ -3,20 +3,46 @@ import axios from 'axios'
 
 export const useBookStore = defineStore('book', {
 	state: () => ({
-		books: {
-			"Beginner": [],
-			"Intermediate": [],
-			"Advanced": [],
+		books: [],
+		bookCount: {
+			beginner: { count: 0, limit: 0 },
+			intermediate: { count: 0,	limit: 0 },
+			advanced: { count: 0,	limit: 0 }
 		},
+		defaultLimit: 6
 	}),
+	getters: {
+		getBooksByLevel: (state) => (level) =>{
+			return state.books.filter(book => book.level == level)
+		},
+
+		getActiveBookLevels: (state) => {
+			return Object.keys(state.bookCount).filter(level => state.bookCount[level].count > 0)
+		},
+
+		getBookLevels(state) {
+			return Object.keys(state.bookCount)
+		}
+	},
+
 	actions: {
-		async fetchBooks(level, limit) {
+		async fetchBooks(level, limit = this.defaultLimit) {
 			try{
 				const response = await axios.get(`http://localhost:3000/book/level/${level}/limit/${limit}`)
-				this.books[level] = response.data
+				this.books = this.books.concat(response.data)
+				this.bookCount[level].limit += this.defaultLimit
 			} catch (err) {
 				console.error(err)
 			}
 		},
+
+		async fetchBookCount(level) {
+			try{
+				const response = await axios.get(`http://localhost:3000/book/level/${level}/count`)
+				this.bookCount[level].count = response.data
+			} catch (err) {
+				console.error(err)
+			}
+		}
 	},
 })
