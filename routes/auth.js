279be6e21e46a1d8375fc9af4dac7475df17d73a
aksuharun <harun16aksu@gmail.com>
2024-07-16 00:17:24 +0300

    Update auth router

--- routes/auth.js
+++ routes/auth.js
@@ -51,7 +51,6 @@ router.post('/login', async (req, res) => {
 			res.json({ msg: 'Logic Success' })
 		})
 		.catch(err =>{
-			console.log(err)
 			res.status(500).json({ err: err.message })
 	})
 })
